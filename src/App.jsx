dd17b052acfd7046b999585c66684270f2871746
playck <playck@naver.com>
2022-07-20 00:00:39 +0900

    Add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <div className="App">리액트 테스트</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Navbar extends PureComponent {
+  render() {
+    return (
+      <nav>
+        <span>To-Do-List</span>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
