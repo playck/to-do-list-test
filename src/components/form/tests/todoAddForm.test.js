e379398ec47b303b0d60feda6694ca80370b791b
playck <playck@naver.com>
2022-08-21 20:20:47 +0900

    Add todoAddForm snapshot test

--- src/components/form/tests/todoAddForm.test.js
+++ src/components/form/tests/todoAddForm.test.js
@@ -1,33 +1,43 @@
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TodoAddForm from "../todoAddForm";
+import renderer from "react-test-renderer";
 
-describe("Form Submit", () => {
-  let handleAddTodo;
-  let input;
-  let button;
-
-  beforeEach(() => {
-    handleAddTodo = jest.fn();
-    render(<TodoAddForm handleAddTodo={handleAddTodo} />);
-    input = screen.getByPlaceholderText("할 일은?");
-    button = screen.getByText("등록");
+describe("TodoAddForm", () => {
+  it("redners", () => {
+    const component = renderer.create(
+      <TodoAddForm handleAddTodo={jest.fn()} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it("calls AddTodo when button is clicked and input not empty", () => {
-    userEvent.type(input, "New Todo");
-    userEvent.click(button);
+  describe("Form Submit", () => {
+    let handleAddTodo;
+    let input;
+    let button;
 
-    expect(handleAddTodo).toHaveBeenCalledWith("New Todo");
-  });
+    beforeEach(() => {
+      handleAddTodo = jest.fn();
+      render(<TodoAddForm handleAddTodo={handleAddTodo} />);
+      input = screen.getByPlaceholderText("할 일은?");
+      button = screen.getByText("등록");
+    });
 
-  it("does not call AddTodo when the todo is empty", () => {
-    // userEvent.type(input, "");
-    // Error: Expected key descriptor but found "" in ""
-    // falsy를 허용하지 않음
-    userEvent.clear(input);
-    userEvent.click(button);
+    it("calls AddTodo when button is clicked and input not empty", () => {
+      userEvent.type(input, "New Todo");
+      userEvent.click(button);
 
-    expect(handleAddTodo).toHaveBeenCalledTimes(0);
+      expect(handleAddTodo).toHaveBeenCalledWith("New Todo");
+    });
+
+    it("does not call AddTodo when the todo is empty", () => {
+      // userEvent.type(input, "");
+      // Error: Expected key descriptor but found "" in ""
+      // falsy를 허용하지 않음
+      userEvent.clear(input);
+      userEvent.click(button);
+
+      expect(handleAddTodo).toHaveBeenCalledTimes(0);
+    });
   });
 });
